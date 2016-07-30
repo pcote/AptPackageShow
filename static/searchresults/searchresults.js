e9d37f8c58440b5ffe9c745f5ca2c511b0ff1f0f
Phil Cote <cotejrp@gmail.com>
2016-07-30 19:24:37 -0400

    Completed implementation of the search results table view

--- static/searchresults/searchresults.js
+++ static/searchresults/searchresults.js
@@ -1,9 +1,32 @@
 var apsSearchResults = function(){
     var d = {};
     d.restrict = "E";
+    d.scope = {
+        results: "="
+    };
+
     d.templateUrl = "searchresults/searchresults.html";
+    d.controller = "searchResultsController";
+    d.controllerAs = "src";
     return d;
 };
 
+var searchResultsController = function($scope){
+    var src = this;
+    src.packageList = [];
+
+    // Watch set up for changes in the package list that comes up.
+    var resVal = function(){
+        return $scope.results;
+    };
+
+    var resListener = function(newVal, oldVal){
+        src.packageList = $scope.results;
+    };
+
+    $scope.$watch(resVal, resListener);
+};
+
 angular.module("searchresults", [])
+    .controller("searchResultsController", searchResultsController)
     .directive("apsSearchResults", apsSearchResults);
