5c5b0cc83d284bb4636690d1c649ae25ffdceb5a
Phil Cote <cotejrp@gmail.com>
2016-08-03 11:16:29 -0400

    No reason for service code to be separated from the rest of the js.  Merging services into their proper js files therefore.

--- static/repostats/repostats.js
+++ static/repostats/repostats.js
@@ -1,3 +1,14 @@
+var RepoStatsService = function($http){
+
+    var svc = this;
+
+    svc.getStats = function(){
+        var promise = $http.get("/stats");
+        return promise;
+    };
+
+};
+
 var apsRepoStats = function(){
     var d = {};
     d.restrict = "E";
--- static/searchbox/searchbox.js
+++ static/searchbox/searchbox.js
@@ -1,3 +1,14 @@
+var SearchBoxService = function($http){
+    var sbs = this;
+
+    sbs.doSearch = function(term){
+        var escapedTerm = escape(term);
+        var url = "/search/" + escapedTerm
+        var promise = $http.get(url);
+        return promise;
+    };
+};
+
 var apsSearchBox = function(){
     var d = {};
     d.restrict = "E";
